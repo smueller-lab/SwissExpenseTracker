55494704f13d24cac7f1f3697444dea2da82ac30
Sebastian <sebastian96mueller@gmail.com>
2025-12-04 20:12:18 +0100

    Create new groceries page, insert figures and clean layout and use functions

--- app/app.py
+++ app/app.py
@@ -1,13 +1,10 @@
 import dash
 from dash import html, dcc, Input, Output, dash_table
-import plotly.express as px
 from plotly.io import from_json
 import pandas as pd
 from config import FDP, VIS
 fdp = FDP()
 vis = VIS()
-
-
 
 # ----- load Data -----
 pdf = pd.read_parquet(fdp.pth_Master_BankZKB)
@@ -19,15 +16,42 @@ scol_TopExpenses = [{'name': col, 'id': col, **vis.vk_format_col.get(col, {'type
 
 Balance = pdf.sort_values(by='Date', ascending=False)['Balance_CHF'].iloc[0]
 
-
-# ----- load figures -----
-with open(fdp.pth_fig_BalancePerDay) as f:
-    fig_balance = from_json(f.read())
-
-
 # ----- functions -----
 def get_balance_class(value):
     return "card-value balance-positive" if value > 0 else "card-value balance-negative"
+
+
+def make_figure_card(title: str, pth_fig: str):
+    with open(pth_fig, "r") as f:
+        fig = from_json(f.read())
+
+    return html.Div([
+        html.H6(title, className="graph-title"),
+        dcc.Graph(figure=fig)
+    ], className="graph-container")
+
+
+def make_table_card(title: str, s_col: list, data: dict, table_id: str):
+    """make table card
+
+    Args:
+        title (str): title for the Card
+        s_col (list): columns to show from the table
+        data (dict): data
+        table_id (str): table id
+    """
+
+    return html.Div([
+        html.H6(title, className="graph-title"),
+        dash_table.DataTable(
+            id=table_id,
+            columns=s_col,
+            data=data,
+            style_table={'overflowX': 'auto'}
+        )
+    ], className="graph-container")
+
+
 
 # ---------- Initialize App ----------
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
@@ -39,12 +63,15 @@ app.layout = html.Div([
     html.Div([
         html.H2("Expense Tracker", className="logo"),
         html.Div([
-            dcc.Link("🏠 Home", href="/", className="menu-link active"),
-            dcc.Link("🍽️ Food Analytics", href="/food", className="menu-link"),
-            dcc.Link("✈️ Transport Analytics", href="/transport", className="menu-link"),
-            dcc.Link("⛳ Sport Analytics", href="/sport", className="menu-link"),
+            dcc.Link("🏠 Home", href="/", id="link-home", className="menu-link"),
+            dcc.Link("🛒 Groceries", href="/groceries", id="link-groceries", className="menu-link"),
+            dcc.Link("🍽️ Restaurant & Bars", href="/restaurant", id="link-restaurant", className="menu-link"),
+            dcc.Link("✈️ Transport Analytics", href="/transport", id="link-transport", className="menu-link"),
+            dcc.Link("⛳ Sport Analytics", href="/sport", id="link-sport", className="menu-link"),
         ], className="menu"),
     ], className="sidebar"),
+
+    dcc.Location(id="url", refresh=False),
 
     # Page Content
     html.Div(id="page-content", className="content")
@@ -75,22 +102,24 @@ home_layout = html.Div([
         ], className="card"),
     ], className="cards-container"),
 
-    html.Div([
-        html.Div([
-            html.H3('Balance Progression', className='graph-title'),
-            dcc.Graph(figure=fig_balance)
-        ], className="graph-container"),
+    
+    make_figure_card("Balance Progression", fdp.pth_fig_BalancePerDay),
+    make_table_card(
+        title='Top 20 Expenses',
+        s_col=scol_TopExpenses,
+        data=pdf_TopExpenses.to_dict('records'),
+        table_id="table-top-expenses"
+    ),
+    
+])
 
-        html.Div([
-            html.H3('Top 20 Expenses', className='graph-title'),
-            dash_table.DataTable(
-                id='table-top-expenses',
-                columns=scol_TopExpenses,
-                data=pdf_TopExpenses.to_dict('records'),
-                style_table={'overflowX': 'auto'},
-            )
-        ], className='graph-container')
-    ])
+
+# --------- Grocery Page ----------
+groceries_layout = html.Div([
+    html.H2("🛒 Groceries Analytics", className="page-title-center"),
+    make_figure_card("Grocery Expenses per month", fdp.pth_fig_GroceryBar),
+    make_figure_card("Grocery Expense Merchant distribution per month [%]", fdp.pth_fig_GroceryBarPct),
+    make_figure_card("Total Grocery Expenses vs. fraction of Merchant from total expenses", fdp.pth_fig_GroceryLR)
 ])
 
 # Placeholder pages
@@ -103,10 +132,40 @@ settings_layout = html.Div([
 ])
 
 # ---------- Callbacks ----------
-@app.callback(Output("page-content", "children"),
-              [Input("page-content", "id")])
-def display_page(_):
-    return home_layout  # Only the Home page for now
+@app.callback(
+    Output("page-content", "children"),
+    Input("url", "pathname")
+)
+def display_page(pth):
+    if pth == "/":
+        return home_layout
+    elif pth == '/groceries':
+        return groceries_layout
+    
+
+@app.callback(
+    [
+        Output("link-home", "className"),
+        Output("link-groceries", "className"),
+        Output("link-restaurant", "className"),
+        Output("link-transport", "className"),
+        Output("link-sport", "className")
+    ],
+    Input("url", "pathname")
+)
+def highlight_active_tab(pth):
+
+    default = "menu-link"
+    active = "menu-link active"
+
+    return [
+        active if pth == "/" else default,
+        active if pth == "/groceries" else default,
+        active if pth == "/restaurant" else default,
+        active if pth == "/transport" else default,
+        active if pth == "/sport" else default,
+    ]
+
 
 
 if __name__ == "__main__":
